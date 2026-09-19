ee2b3c9e178b61a3a8c109a1429282a3049fa3ee
brunofoxmulder <bruno@mbsimon.fr>
2026-09-19 15:04:49 +0200

    dev.6: route Memory resolver tool in conversation.py

--- custom_components/elise_live/conversation.py
+++ custom_components/elise_live/conversation.py
@@ -56,6 +56,7 @@ from .stt import (
 )
 from .memory import (
     MEMORY_SEARCH_TOOL_NAME,
+    MEMORY_RESOLVE_TOOL_NAME,
     add_memory_instruction,
     add_memory_tool,
     assistant_confirms_wake_greeting,
@@ -327,6 +328,10 @@ class LiveModelConversationAgent(conversation.ConversationEntity):
                                     }
                                 elif tool_name == MEMORY_SEARCH_TOOL_NAME:
                                     tool_result = await memory_bridge.async_search(
+                                        str(tool_args.get("query", ""))
+                                    )
+                                elif tool_name == MEMORY_RESOLVE_TOOL_NAME:
+                                    tool_result = await memory_bridge.async_resolve_entity(
                                         str(tool_args.get("query", ""))
                                     )
                                 elif llm_api is not None:
